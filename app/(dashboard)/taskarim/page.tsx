6f984e96b34482fb1b4d2a221e070dd830ff3459
Saleh <seyed.saleh.niknejad@gmail.com>
2024-08-29 13:36:03 +0330

    fix: font family inherit fixed

--- app/(dashboard)/taskarim/page.tsx
+++ app/(dashboard)/taskarim/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function page() {
   return (
     <div>
-      <h1 className="">تسکریم پلتفرم مدیریت تسک هوشمند</h1>
+      <h1 className="text-2xl">تسکریم پلتفرم مدیریت تسک هوشمند</h1>
     </div>
   );
 }
