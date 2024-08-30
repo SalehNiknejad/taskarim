0f17dacfaa8f6c3143320d30820b7a1b2e7b3706
Saleh <seyed.saleh.niknejad@gmail.com>
2024-08-31 00:11:52 +0330

    feat: use taskcard in page taskarim

--- app/(dashboard)/taskarim/page.tsx
+++ app/(dashboard)/taskarim/page.tsx
@@ -1,10 +1,20 @@
+import TaskCard from "@/app/components/TaskCard/TaskCard";
 import React from "react";
 
 function page() {
   return (
-    <div>
-      <h1 className="text-2xl">تسکریم پلتفرم مدیریت تسک هوشمند</h1>
-    </div>
+    <main>
+      <h1 className="text-2xl mb-10">تسکریم پلتفرم مدیریت تسک هوشمند</h1>
+      <div className="flex-wrap flex gap-6 ">
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard /> <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+      </div>
+    </main>
   );
 }
 
