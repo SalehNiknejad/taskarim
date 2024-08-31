5fc8e302e66c71fa1414ac781cbeef5ed981dba0
S.Niknejad <S.Niknejad@flightio.com>
2024-08-31 18:51:26 +0330

    feat: add supabase to signup page

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,27 @@
+"use client";
 import AuthForm from "@/app/components/Auth/AuthForm";
 import React from "react";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { createClient } from "@/app/utils/supabase/server";
 
-function signup() {
+async function signup() {
+  const supabase = createClient();
+
+  const sa = {
+    email: "saleh@gmail.com",
+    password: "123456",
+  };
+  const { data, error } = await supabase.auth.signInWithPassword(sa);
+
+  if (error) {
+    // redirect("/error");
+    console.log(error);
+  }
+  console.log(data);
+  // revalidatePath("/", "layout");
+  // redirect("/");
+
   return <AuthForm title="ثبت نام کاربر" createAccont={false} />;
 }
 
